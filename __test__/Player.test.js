8819b867320c00c4af7265dc504087c76a212a82
Blake Smethers <smethersblake@gmail.com>
2022-04-30 11:40:37 -0500

    add player health

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -30,3 +30,22 @@ test('gets inventory from player or returns false', () => {
     player.inventory = []
     expect(player.getInventory()).toEqual(false)
 });
+test('gets players health value', () => {
+    const player = new Player('Dave')
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()))
+});
+test('checks if player is alive', () => {
+    const player = new Player('Dave')
+    expect(player.isAlive()).toBeTruthy()
+
+    player.health = 0 
+    expect(player.isAlive()).toBeFalsy()
+});
+test('subtracts from players health', () => {
+    const player = new Player('Dave')
+    const oldHealth = player.health
+    player.reduceHealth(5)
+    expect(player.health).toBe(oldHealth - 5)
+    player.reduceHealth(99999999)
+    expect(player.health).toBe(0)
+});
